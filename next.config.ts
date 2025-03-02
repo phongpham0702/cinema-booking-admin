77e265d4fdc6a4bdeb0c349982f74519bc7440cc
phongpham0702 <thanhphong07022002@gmail.com>
2025-03-02 16:30:23 +0700

    chore: setup next config

--- next.config.ts
+++ next.config.ts
@@ -4,24 +4,48 @@ import type { NextConfig } from 'next';
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
+
 const nextConfig: NextConfig = {
   output: 'standalone',
+
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '**' }],
   },
-  
-  webpack(config) {
+
+  webpack(config, { isServer }) {
     config.module.rules.push({
       test: /\.svg$/i,
       use: ['@svgr/webpack'],
     });
 
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+      };
+    }
+
     return config;
   },
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'X-Frame-Options', value: 'DENY' }, // prevent click-jacking
+          { key: 'X-XSS-Protection', value: '1; mode=block' }, // prevent XSS
+          { key: 'X-Content-Type-Options', value: 'nosniff' }, // prevent sniffing MIME types
+        ],
+      },
+    ];
+  },
+
   experimental: {
     serverActions: {
       allowedOrigins: [''],
     },
+
     turbo: {
       rules: {
         '*.svg': {
@@ -30,9 +54,13 @@ const nextConfig: NextConfig = {
         },
       },
     },
+
     authInterrupts: true,
+
     optimizePackageImports: ['antd'],
   },
+
+  reactStrictMode: false,
 };
 
 export default withBundleAnalyzer({ ...nextConfig });
